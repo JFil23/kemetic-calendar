463c5e082e589be310d54f84abeb91f9131c4fff
J Phil <jphil@yahoo.com>
2026-09-16 14:42:11 -0700

    Fix Day View keyboard ownership

--- lib/features/calendar/presentation/instrument_event_presentation_frame.dart
+++ lib/features/calendar/presentation/instrument_event_presentation_frame.dart
@@ -32,26 +32,16 @@ double instrumentEventSheetExtentForViewport({
 Future<T?> showCalendarEventDetailSheetModal<T>({
   required BuildContext context,
   required WidgetBuilder builder,
-  bool editable = false,
 }) {
-  if (editable) {
-    return showEditableModalBottomSheet<T>(
-      context: context,
-      backgroundColor: Colors.transparent,
-      isDismissible: true,
-      enableDrag: true,
-      useRootNavigator: false,
-      constrainMediaSizeToAvailableHeight: true,
-      builder: builder,
-    );
-  }
   return showModalBottomSheet<T>(
     context: context,
     backgroundColor: Colors.transparent,
     isScrollControlled: true,
     isDismissible: true,
     enableDrag: true,
+    useSafeArea: true,
     useRootNavigator: false,
+    requestFocus: true,
     builder: builder,
   );
 }
