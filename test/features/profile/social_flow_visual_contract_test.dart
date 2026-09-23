2b59247805f16c3375abb141e5e1af239a611d8f
J Phil <jphil@yahoo.com>
2026-09-22 21:58:00 -0700

    Register exact Linux social artifact golden

--- test/features/profile/social_flow_visual_contract_test.dart
+++ test/features/profile/social_flow_visual_contract_test.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi' show Abi;
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -5,6 +8,18 @@ import 'package:mobile/data/flow_appearance.dart';
 import 'package:mobile/features/profile/posted_flow_artifact.dart';
 
 import '../../support/maat_flow_visual_test_fonts.dart';
+
+String get _socialFlowArtifactsGolden {
+  if (!Platform.isLinux) {
+    return 'goldens/social_flow_artifacts.png';
+  }
+  return switch (Abi.current()) {
+    Abi.linuxX64 => 'goldens/linux-x64/social_flow_artifacts.png',
+    final abi => throw UnsupportedError(
+      'No exact social flow artifact golden is registered for $abi.',
+    ),
+  };
+}
 
 void main() {
   setUpAll(() async {
@@ -80,7 +95,7 @@ void main() {
 
       await expectLater(
         find.byKey(const ValueKey<String>('social-flow-visual-contract')),
-        matchesGoldenFile('goldens/social_flow_artifacts.png'),
+        matchesGoldenFile(_socialFlowArtifactsGolden),
       );
     },
   );
